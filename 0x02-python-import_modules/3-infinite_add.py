5caa70dd44f2c5a12c4d78fc9c46590aff61698d
Enoch <henokwon@gmail.com>
2022-12-26 03:25:06 -0500

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    arg_num = len(argv) - 1
+    if arg_num == 0:
+        print("{}".format(arg_num))
+    else:
+        result = []
+        for i in range(1, arg_num + 1):
+            result.append(int(argv[i]))
+        print("{}".format(sum(result)))
